183241a5d16271e6b32b6e0e45f9ae57f6d8a261
Filippo Brogi <s345969@studenti.polito.it>
2025-11-09 12:31:25 +0100

    Updated README and created block EX, still to test

--- src/a.b-DP-EX.vhd
+++ src/a.b-DP-EX.vhd
@@ -1,17 +1,29 @@
 library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
+  use IEEE.std_logic_1164.all;
+  use IEEE.numeric_std.all;
+  use work.myTypes.all;
+  use work.rf_pkg.all;
 
 entity DP_EX is
   generic (
     IR_SIZE         : INTEGER := 32; -- Instruction size.
     OPERAND_SIZE    : INTEGER := 5;  -- Source / Destination Operand Size
     I_TYPE_IMM_SIZE : INTEGER := 16; -- Immediate Bit Field Size for I-Type Instruction
-    J_TYPE_IMM_SIZE : INTEGER := 26  -- Immediate Bit Field Size for J-Type Instruction
+    J_TYPE_IMM_SIZE : INTEGER := 26; -- Immediate Bit Field Size for J-Type Instruction
+    RF_regBits      : integer := 32; -- Bitwidth of RF words
+    RF_numRegs      : integer := 32  -- Number of RF registers
   );
   port (
-    CLK          : in    std_logic;                              -- Clock
-    nRST         : in    std_logic;                              -- nRST:Active-Low
+    -- Inputs
+    CLK  : in    std_logic; -- Clock
+    nRST : in    std_logic; -- nRST:Active-Low
+
+    -- Inputs from IF+ID Block
+    S2_REG_ADD_WR_OUT : in    std_logic_vector(OPERAND_SIZE - 1 downto 0); -- Part of sequence of registers at Write-Address input of Register File.
+    S2_RFILE_A_OUT    : in    std_logic_vector(IR_SIZE - 1 downto 0);      -- RFILE = Register File
+    S2_RFILE_B_OUT    : in    std_logic_vector(IR_SIZE - 1 downto 0);
+    S2_REG_SE_IMM_OUT : in    std_logic_vector(IR_SIZE - 1 downto 0);
+    S2_REG_UE_IMM_OUT : in    std_logic_vector(IR_SIZE - 1 downto 0);
 
     -- Execute (STAGE 3) input signals
     MUX_A_SEL     : in    std_logic;
@@ -19,14 +31,19 @@ entity DP_EX is
     ALU_OUTREG_EN : in    std_logic;
     EQ_COND       : in    std_logic;
     JMP           : in    std_logic;
-    EQZ_NEQZ      : in    std_logic
+    EQZ_NEQZ      : in    std_logic;
 
-  -- Outputs
+    -- Outputs
+    S3_REG_NPC_OUT    : out   std_logic_vector(IR_SIZE - 1 downto 0);
+    S3_FF_JAL_EN_OUT  : out   std_logic; -- Part of sequence of Flip-Flops which connect to the select signal of the MUX in Stage 5.
+    S3_REG_ADD_WR_OUT : out   std_logic_vector(4 downto 0);
+    S3_FF_COND_OUT    : out   std_logic; -- Output of S3_REG_COND register
+    S3_REG_ALU_OUT    : out   std_logic_vector(IR_SIZE - 1 downto 0);
+    S3_REG_DATA_OUT   : out   std_logic_vector(IR_SIZE - 1 downto 0)
   );
 end entity DP_EX;
 
 architecture structural of DP_EX is
-
 
   -- ***********************************************************************
   -- *********************** COMPONENT DECLARATIONS ************************
@@ -61,8 +78,8 @@ architecture structural of DP_EX is
   component gen_register_file is
 
     generic (
-      regBits : integer := 32;
-      numRegs : integer := 32
+      RF_regBits : integer := 32;
+      RF_numRegs : integer := 32
     );
     port (
       -- INPUTS
@@ -71,17 +88,17 @@ architecture structural of DP_EX is
       ENABLE  : in    std_logic;
       RD1     : in    std_logic;
       RD2     : in    std_logic;
-      ADD_RD1 : in    std_logic_vector(log2(numRegs) - 1 downto 0);
-      ADD_RD2 : in    std_logic_vector(log2(numRegs) - 1 downto 0);
+      ADD_RD1 : in    std_logic_vector(log2(RF_numRegs) - 1 downto 0);
+      ADD_RD2 : in    std_logic_vector(log2(RF_numRegs) - 1 downto 0);
 
       -- Write operation
       WR     : in    std_logic;
-      ADD_WR : in    std_logic_vector(log2(numRegs) - 1 downto 0);
-      DATAIN : in    std_logic_vector(regBits - 1 downto 0);
+      ADD_WR : in    std_logic_vector(log2(RF_numRegs) - 1 downto 0);
+      DATAIN : in    std_logic_vector(RF_regBits - 1 downto 0);
 
       -- OUTPUTS
-      OUT1 : out   std_logic_vector(regBits - 1 downto 0);
-      OUT2 : out   std_logic_vector(regBits - 1 downto 0)
+      OUT1 : out   std_logic_vector(RF_regBits - 1 downto 0);
+      OUT2 : out   std_logic_vector(RF_regBits - 1 downto 0)
     );
   end component gen_register_file;
 
@@ -164,6 +181,135 @@ architecture structural of DP_EX is
 
 begin
 
+  -- ****************************************************************************************
+  -- ******************************** STAGE 3 INSTANTIATIONS ********************************
+  -- ****************************************************************************************
 
+  S3_MUX_A : component NBit_2to1MUX
+    generic map (
+      N => IR_SIZE
+    )
+    port map (
+      A => S2_RFILE_A_OUT,
+      B => S1_REG_NPC_OUT,
+      S => MUX_A_SEL,
+      Y => S3_MUX_A_OUT
+    );
+
+  S3_MUX_B : component NBit_2to1MUX
+    generic map (
+      N => IR_SIZE
+    )
+    port map (
+      A => S3_MUX_JMP_OUT,
+      B => S2_RFILE_B_OUT,
+      S => MUX_B_SEL,
+      Y => S3_MUX_B_OUT
+    );
+
+  S3_MUX_JMP : component NBit_2to1MUX
+    generic map (
+      N => IR_SIZE
+    )
+    port map (
+      A => S2_REG_UE_IMM_OUT,
+      B => S2_REG_SE_IMM_OUT,
+      S => JMP,
+      Y => S3_MUX_JMP_OUT
+    );
+
+  S3_ALU : component ALU
+    generic map (
+      IR_SIZE => IR_SIZE
+    )
+    port map (
+      A          => S3_MUX_A_OUT,
+      B          => S3_MUX_B_OUT,
+      ALU_OPCODE => DP_ALU_OPCODE,
+      Y          => S3_ALU_OUT
+    );
+
+  S3_REG_ALU : component NBit_Reg
+    generic map (
+      N => IR_SIZE
+    )
+    port map (
+      CLK   => CLK,
+      nRST  => nRST,
+      LD_EN => ALU_OUTREG_EN,
+      D     => S3_ALU_OUT,
+      Q     => S3_REG_ALU_OUT
+    );
+
+  S3_REG_DATA : component NBit_Reg
+    generic map (
+      N => IR_SIZE
+    )
+    port map (
+      CLK   => CLK,
+      nRST  => nRST,
+      LD_EN => '1',
+      D     => S2_RFILE_B_OUT,
+      Q     => S3_REG_DATA_OUT
+    );
+
+  S3_REG_COND : component NBit_Reg
+    generic map (
+      N => 1
+    )
+    port map (
+      CLK   => CLK,
+      nRST  => nRST,
+      LD_EN => EQ_COND,
+      D(0)  => S3_BranchTaken,
+      Q(0)  => S3_FF_COND_OUT
+    );
+
+  S3_ZeroCompa : component ZeroCompa
+    generic map (
+      IR_SIZE => IR_SIZE
+    )
+    port map (
+      NUMtoCHECK => S2_RFILE_A_OUT,
+      EQZ        => EQZ_NEQZ,
+      JMP        => JMP,
+      RESULT     => S3_BranchTaken
+    );
+
+  S3_FF_JAL_EN : component NBit_Reg
+    generic map (
+      N => 1
+    )
+    port map (
+      CLK   => CLK,
+      nRST  => nRST,
+      LD_EN => '1',
+      D(0)  => S2_FF_JAL_EN_OUT,
+      Q(0)  => S3_FF_JAL_EN_OUT
+    );
+
+  S3_REG_NPC : component NBit_Reg
+    generic map (
+      N => IR_SIZE
+    )
+    port map (
+      CLK   => CLK,
+      nRST  => nRST,
+      LD_EN => '1',
+      D     => S2_REG_NPC_OUT,
+      Q     => S3_REG_NPC_OUT
+    );
+
+  S3_REG_ADD_WR : component NBit_Reg
+    generic map (
+      N => OPERAND_SIZE
+    )
+    port map (
+      CLK   => CLK,
+      nRST  => nRST,
+      LD_EN => '1',
+      D     => S2_REG_ADD_WR_OUT,
+      Q     => S3_REG_ADD_WR_OUT
+    );
 
 end architecture structural;
