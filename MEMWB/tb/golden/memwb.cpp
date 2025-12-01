1fac326d061a654a219e7d34fc4101248c43d222
Mateus Cancado <s346294@studenti.polito.it>
2025-12-01 07:52:02 +0100

    Added systemc golden model

---
+++ MEMWB/tb/golden/memwb.cpp
@@ -0,0 +1,194 @@
+#include <systemc>
+#define IR_SIZE 32
+using namespace sc_core;
+
+SC_MODULE(MemStage) {
+    sc_in<sc_bool>              clk;
+    sc_in<sc_bool>              rst_n;
+
+    sc_in<sc_uint<IR_SIZE>>     s3_reg_npc_out;
+    sc_in<sc_bool>              s3_ff_jal_en_out;
+    sc_in<sc_uint<4>>           s3_reg_add_wr_out;
+    sc_in<sc_bool>              s3_reg_cond_out;
+    sc_in<sc_bool>              jump_en;
+    sc_in<sc_uint<IR_SIZE>>     s1_add_out; //??
+    sc_in<sc_uint<IR_SIZE>>     s3_reg_alu_out;
+    sc_in<sc_uint<IR_SIZE>>     dram_out;
+
+    sc_out<sc_uint<IR_SIZE>>    s4_reg_npc_out;
+    sc_out<sc_bool>             s4_ff_jal_en_out;
+    sc_out<sc_uint<IR_SIZE>>    dp_to_dlx_pc;
+    sc_out<sc_uint<4>>          s4_reg_add_wr_out;
+    sc_out<sc_uint<IR_SIZE>>    s4_reg_alu_out;
+    sc_out<sc_uint<IR_SIZE>>    s4_reg_lmd_out;
+
+    SC_CTOR(MemStage) {
+        SC_METHOD(s4_reg_npc);
+        sensitive << clk << rst_n;
+
+        SC_METHOD(s4_ff_jal_en);
+        sensitive << clk << rst_n;
+
+        SC_METHOD(s4_reg_add_wr);
+        sensitive << clk << rst_n;
+
+        SC_METHOD(s4_mux_jmp);
+        sensitive << s3_reg_cond_out
+                  << jump_en
+                  << s1_add_out
+                  << s3_reg_alu_out;
+
+        SC_METHOD(s4_reg_alu);
+        sensitive << clk << rst_n;
+
+        SC_METHOD(s4_reg_lmd);
+        sensitive << clk << rst_n;
+    }
+
+    void s4_reg_npc() {
+        if (!rst_n)
+            s4_reg_npc_out = 0;
+        else
+            s4_reg_npc_out = s3_reg_npc_out;
+    }
+
+    void s4_ff_jal_en() {
+        if (!rst_n)
+            s4_ff_jal_en_out = 0;
+        else
+            s4_ff_jal_en_out = s3_ff_jal_en_out;
+    }
+
+    void s4_reg_add_wr() {
+        if (!rst_n)
+            s4_reg_add_wr_out = 0;
+        else
+            s4_reg_add_wr_out = s3_reg_add_wr_out;
+    }
+
+    void s4_mux_jmp() {
+        if (s3_reg_cond_out && jump_en)
+            dp_to_dlx_pc = s1_add_out;
+        else
+            dp_to_dlx_pc = s3_reg_alu_out;
+    }
+
+    void s4_reg_alu() {
+        if (!rst_n)
+            s4_reg_alu_out = 0;
+        else
+            s4_reg_alu_out = s3_reg_alu_out;
+    }
+
+    void s4_reg_lmd() {
+        if (!rst_n)
+            s4_reg_lmd_out: = 0;
+        else
+            s4_reg_lmd_out = dram_out;
+    }
+}
+
+SC_MODULE(WBStage) {
+    //sc_in<sc_bool>              clk;
+    //sc_in<sc_bool>              rst_n;
+
+    sc_signal<sc_uint<IR_SIZE>> s5_mux_wb;
+
+    sc_in<sc_uint<IR_SIZE>>     s4_reg_npc_out;
+    sc_in<sc_bool>              s4_ff_jal_en_out;
+    sc_in<sc_uint<IR_SIZE>>     s4_reg_alu_out;
+    sc_in<sc_uint<IR_SIZE>>     s4_reg_lmd_out;
+    sc_in<sc_bool>              wb_mux_sel;
+
+    sc_out<sc_uint<IR_SIZE>>    s5_mux_datain_out;
+
+    SC_CTOR(WBStage) {
+        SC_METHOD(s5_mux_wb);
+        sensitive << s4_reg_alu_out << s4_reg_lmd_out << wb_mux_sel;
+
+        SC_METHOD(s5_mux_datain);
+        sensitive << s4_ff_jal_en_out << s4_reg_npc_out << s5_mux_wb_out;
+    }
+
+    void s5_mux_wb() {
+        if (wb_mux_sel)
+            s5_mux_wb_out = s4_reg_alu_out;
+        else
+            s4_mux_wb_out = s4_reg_lmd_out;
+    }
+
+    void s5_mux_datain() {
+        if (s5_ff_jal_en)
+            s5_mux_datain_out = s4_reg_npc_out;
+        else
+            s5_mux_datain_out = s5_mux_wb_out;
+    }
+}
+
+/* toplevel module */
+
+SC_MODULE(MemWBStage) {
+    /* Signal declarations */
+    sc_in<sc_bool>              clk;
+    sc_in<sc_bool>              rst_n;
+
+    sc_in<sc_uint<IR_SIZE>>             s3_reg_npc_out;
+    sc_in<sc_bool>                      s3_ff_jal_en_out;
+    sc_in<sc_uint<4>>                   s3_reg_add_wr_out;
+    sc_in<sc_bool>                      s3_reg_cond_out;
+    sc_in<sc_bool>                      jump_en;
+    sc_in<sc_uint<IR_SIZE>>             s1_add_out; //??
+    sc_in<sc_uint<IR_SIZE>>             s3_reg_alu_out;
+    sc_in<sc_uint<IR_SIZE>>             dram_out;
+
+    sc_in<sc_uint<IR_SIZE>>             wb_mult_sel;
+
+    sc_signal<sc_uint<IR_SIZE>>         s4_reg_npc_out;
+    sc_signal<sc_bool>                  s4_ff_jal_en_out;
+    sc_signal<sc_uint<IR_SIZE>>         s4_reg_alu_out;
+    sc_signal<sc_uint<IR_SIZE>>         s4_reg_lmd_out;
+
+    /* Output signal declarations */
+    sc_out<sc_uint<IR_SIZE>>            s4_reg_add_wr_out;
+    sc_out<sc_uint<IR_SIZE>>            dp_to_dlx_pc;
+    sc_out<sc_uint<IR_SIZE>>            s5_mux_datain_out;
+
+    /* Submodule declaration */
+    MemStage    mem;
+    WBStage     wb;
+
+    SC_CTOR(MemWBStage) : mem("Mem Stage")
+                        , wb("Writeback Stage")
+    {
+        /* Mem stage input signals */
+        mem.clk(clk);
+        mem.rst_n(rst_n);
+
+        mem.s3_reg_npc_out(s3_reg_npc_out);
+        mem.s3_ff_jal_en_out(s3_ff_jal_en_out);
+        mem.s3_reg_add_wr_out(s3_reg_add_wr_out);
+        mem.s3_reg_cond_out(s3_reg_cond_out);
+        mem.jump_en(jump_en);
+        mem.s1_add_out(s1_add_out);
+        mem.s3_reg_alu_out(s3_reg_alu_out);
+        mem.dram_out(dram_out);
+        
+        /* Mem stage output signals */
+        mem.s4_reg_npc_out(s4_reg_npc_out);
+        mem.s4_ff_jal_en_out(s4_ff_jal_en_out);
+        mem.dp_to_dlx_pc(dp_to_dlx_pc);
+        mem.s4_reg_add_wr_out(s4_reg_add_wr_out);
+        mem.s4_reg_alu_out(s4_reg_alu_out);
+        mem.s4_reg_lmd_out(s4_reg_lmd_out);
+
+        /* Wb stage input signals */
+        wb.s4_reg_npc_out(s4_reg_npc_out);
+        wb.s4_ff_jal_en_out(s4_ff_jal_en_out);
+        wb.s4_reg_alu_out(s4_reg_alu_out);
+        wb.s4_reg_lmd_out(s4_reg_lmd_out);
+        wb.wb_mux_sel(wb_mux_sel);
+
+        /* Wb stage output signals */
+        wb.s5_mux_datain_out(s5_mux_datain_out);
+    }
+}
