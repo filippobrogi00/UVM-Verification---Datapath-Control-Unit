33a0e1865d0c5ebf25577b3eade0ad3076e28ecc
Mateus Cancado <s346294@studenti.polito.it>
2025-12-11 14:21:57 +0100

    FEAT: Implemented mechanism for stuckat faults

--- MEMWB/sim/run_qrun.sh
+++ MEMWB/sim/run_qrun.sh
@@ -5,16 +5,25 @@ TB_DIR=../tb
 GM_DIR=../tb/golden
 COV_EXCLUDE="work.rf_pkg work.Module_topTestbench work.Module_topTestbench_sv_unit"
 SEED=0
+DUT_HIERARCHY="/Module_topTestbench/memwb_toplevel/DUT"
+FAULT_LIST="stuckat_fault_list.txt"
+FAULT_DURATION="100"
 
 SRC_FILES=$(find $SRC_DIR -maxdepth 1 -name "*.vhd")
-#SV_COMPILE_LIST="$TB_DIR/Module_GoldenModel.sv $TB_DIR/Iface_MEMWB.sv $TB_DIR/Module_MEMWB_Wrapper.sv $TB_DIR/Module_topTestbench.sv"
-#SV_COMPILE_LIST="$TB_DIR/Iface_MEMWB.sv $GM_DIR/Iface_GoldenModel.sv $TB_DIR/Module_MEMWB_Wrapper.sv $TB_DIR/Module_topTestbench.sv"
 SV_COMPILE_LIST="$(find $TB_DIR -maxdepth 1 -name "*.sv") $(find $GM_DIR -maxdepth 1 -name "*.sv")"
 TOPLEVEL="Module_topTestbench"
 #GM_FILES=$(find $GM_DIR -maxdepth 1 -name "*.cpp")
-GM_FILES=$(find $GM_DIR -maxdepth 1 -name "*.c")
+#GM_FILES=$(find $GM_DIR -maxdepth 1 -name "*.c")
 COV_EXCLUDE_COMMAND=$(printf "coverage exclude -du %s; " $COV_EXCLUDE)
-echo $COV_EXCLUDE_COMMAND
+FORCE_COMMAND=""
+
+for i in {1..2}
+do
+    FAULT=$(awk -F' ' "NR==$i {print \$1}" $FAULT_LIST | sed "s/sa//")
+    SIGNAL=$(awk -F' ' "NR==$i {print \$3}" $FAULT_LIST)
+    FORCE_COMMAND="$FORCE_COMMAND; force -freeze $DUT_HIERARCHY/$SIGNAL $FAULT $(($FAULT_DURATION*($i - 1))) -cancel $(($FAULT_DURATION*$i))"
+done
+echo $FORCE_COMMAND
 
 ###################
 ###### USAGE ######
@@ -42,7 +51,6 @@ else
   source "/eda/scripts/init_questa_core_prime"
   # Setsynopsys
   source "/eda/scripts/init_design_vision"
-
 fi
 
 # Utilities for output coloring and systemverilog functions
@@ -52,5 +60,6 @@ source ./systemverilog_utils.sh # get_systemverilog_testbench_module()
 #qrun -clean -coverage -uvm -autoorder -mixedsvvh $PACKAGES $SRC_FILES $SV_COMPILE_LIST $C_FILES -sysc $GM_FILES -top $TOPLEVEL
 rm -rf cov_$SEED.ucdb covhtmlreport
 
-qrun -clean -uvm -autoorder -mixedsvvh -coverage +cover=sbce  $SRC_FILES $SV_COMPILE_LIST $GM_FILES -do "$COV_EXCLUDE_COMMAND; run -all" -top $TOPLEVEL
-vcover report -details -html cov_$SEED.ucdb
+#qrun -clean -uvm -autoorder -mixedsvvh -coverage +cover=sbce  $SRC_FILES $SV_COMPILE_LIST $GM_FILES -do "$COV_EXCLUDE_COMMAND; run -all" -top $TOPLEVEL
+qrun -clean -uvm -autoorder -mixedsvvh -coverage +cover=sbce  $SRC_FILES $SV_COMPILE_LIST $GM_FILES -do "$FORCE_COMMAND; $COV_EXCLUDE_COMMAND; run -all" -top $TOPLEVEL
+#vcover report -details -html cov_$SEED.ucdb
